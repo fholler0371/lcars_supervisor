f605294f9cf8b9f2db2282f4457e4e567a84b4dd
Frank Holler <holler.frank@gmail.com>
2024-08-04 21:53:19 +0200

    Backup zu AWS S3

--- addons/backup/lib/addon_com/api.py
+++ addons/backup/lib/addon_com/api.py
@@ -2,6 +2,9 @@ from aiohttp import web
 from datetime import datetime as dt
 from os import walk
 import os
+import tomllib
+import boto3
+import time
 
 import clilib.data as cd
 from corelib import BaseObj, Core
@@ -9,20 +12,55 @@ from httplib.models import HttpHandler, HttpRequestData, SendOk
 
 BACKUP_FOLDER = '/lcars/backup'
 
+import logging
+
 class Api(BaseObj):
     def __init__(self, core: Core) -> None:
         BaseObj.__init__(self, core)
         self._apps = {}
         
     async def do_scan(self, idx: int, idx_file: str, rem_folder: str):
+        count = 0
+        with open("/lcars/config/secret.toml", "rb") as f:
+            secret = tomllib.load(f)
+        session = boto3.Session(aws_access_key_id=secret['aws']['key'], aws_secret_access_key=secret['aws']['secret'],
+                                region_name=secret['aws']['region'])
+        s3 = session.resource('s3')
+        bucket = s3.Bucket(secret['aws']['bucket'])
+        try:
+            objs = list(bucket.objects.filter(Prefix=idx_file))
+            if len(objs) == 1:
+                data = objs[0].get()
+                if data['Body'].read().decode() == idx:
+                    self.core.log.debug(f"Backup ({rem_folder}) ist schon vorhanden")
+                    return
+        except Exception as e:
+            self.core.log.error(e)  
+        self.core.log.debug(bucket)    
         for (dirpath, dirnames, filenames) in walk(BACKUP_FOLDER):
             for name in filenames:
-                self.core.log.debug(os.path.join(dirpath, name)[len(BACKUP_FOLDER):])
-        
-        self.core.log.debug(idx_file)   
-        self.core.log.debug(rem_folder)
-        self.core.log.debug(idx)
-       
+                key = os.path.join(dirpath, name)[len(BACKUP_FOLDER):]
+                try:
+                    logging.getLogger('boto').setLevel(logging.CRITICAL)
+                    objs = list(bucket.objects.filter(Prefix=f'{rem_folder}{key}'))
+                    if len(objs) == 0:
+                        logging.getLogger('boto').setLevel(logging.CRITICAL)
+                        s3.meta.client.upload_file(os.path.join(dirpath, name), secret['aws']['bucket'], f'{rem_folder}{key}')
+                        count += 1
+                    else:
+                        logging.getLogger('boto').setLevel(logging.CRITICAL)
+                        days = (objs[0].last_modified.replace(tzinfo=None)-dt.fromtimestamp(os.path.getmtime(os.path.join(dirpath, name)))).days
+                        if days < 10:
+                            logging.getLogger('boto3').setLevel(logging.CRITICAL)
+                            s3.meta.client.upload_file(os.path.join(dirpath, name), secret['aws']['bucket'], f'{rem_folder}{key}')
+                            count += 1
+                except Exception as e:
+                    self.core.log.error(e)  
+        self.core.log.info(f"{count} Dateien gesichert {rem_folder}")
+        try:
+            s3.Object(secret['aws']['bucket'], idx_file).put(Body=idx)
+        except Exception as e:
+            self.core.log.error(e)  
         
     async def do_day(self):
         rem_folder = f'day/{await self.core.web_l.hostname}'
---
+++ addons/backup/src/update.py
@@ -0,0 +1,21 @@
+import asyncio
+import importlib
+
+LIBS = {
+   "boto3" : "boto3"
+}
+
+async def main() -> None:
+    for _import, _check in LIBS.items():
+        try:
+            mod = importlib.import_module(_check)
+        except:
+            cmd = f'pip install {_import}'
+            p = await asyncio.subprocess.create_subprocess_shell(cmd, 
+                                                                #stderr=asyncio.subprocess.PIPE, 
+                                                                stdout=asyncio.subprocess.PIPE)
+            await p.wait()
+            print(f'{_import} wurde installiert')
+
+if __name__ == '__main__':
+    asyncio.run(main())
